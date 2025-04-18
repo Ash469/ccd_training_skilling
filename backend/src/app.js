ea9344189dea580cb4b370ebe15fd51056a615ef
Aryashi <tripathiaryashi@gmail.com>
2025-04-18 23:14:39 +0530

    Add backend email reminder functionality

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const nodemailer = require('nodemailer');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth.routes');
 const eventRoutes = require('./routes/event.routes'); 
+const sendEmailRoutes = require('./routes/sendEmail.routes');
 
 require('dotenv').config();
 
@@ -30,8 +32,11 @@ app.get('/', (req, res) => {
 });
 app.use('/api', authRoutes);
 app.use('/api/events', eventRoutes); // Add this line
+app.use('/api/email', sendEmailRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
